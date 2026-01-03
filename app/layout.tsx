fe446c7c789029d44053df6eab702d01603bfc8c
ZxStim <zxstim@gmail.com>
2026-01-03 12:50:05 +0700

    feat: updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,43 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import type { Metadata } from "next";
 import './global.css';
-import { Inter } from 'next/font/google';
+import { JetBrains_Mono } from "next/font/google";
 
-const inter = Inter({
-  subsets: ['latin'],
-});
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ZxStim",
+  description: "Building and shipping software products",
+  metadataBase: new URL("https://www.zxstim.com"),
+  openGraph: {
+    title: "ZxStim",
+    description: "Building and shipping software products",
+    url: "https://www.zxstim.com",
+    siteName: "ZxStim",
+    images: [
+      {
+        url: "/zxstim-tbn.png",
+        width: 1200,
+        height: 630,
+        alt: "og-image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ZxStim",
+    description: "Building and shipping software products",
+    creator: "@zxstim",
+    images: ["/zxstim-tbn.png"],
+  },
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={jetBrainsMono.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
